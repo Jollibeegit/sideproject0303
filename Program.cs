0e1840b0c3ebf06501b626ce3f0dbf639866c4d2
Jollibeegit <gkrrb6560@gmail.com>
2026-03-03 17:55:22 +0900

    initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using SideProject0303.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// 🔹 Render PORT 대응 (로컬에서는 8080 사용)
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseStatusCodePagesWithReExecute("/not-found");
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
